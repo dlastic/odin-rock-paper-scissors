cb1267a3016282997b7020b12760f0f2e6fd6b5a
dlastic <daniellastic@gmail.com>
2025-01-26 18:26:39 +0100

    Write the logic to play a single round

--- index.js
+++ index.js
@@ -17,4 +17,58 @@ function getHumanChoice() {
   return choice;
 }
 
-console.log(getHumanChoice())
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  if (humanChoice === computerChoice) {
+    console.log("It is a tie!");
+    return;
+  }
+  if (humanChoice === "rock") {
+    if (computerChoice === "paper") {
+      computerScore++;
+      console.log("You lose! Paper beats Rock");
+      return;
+    }
+    if (computerChoice === "scissors") {
+      humanScore++;
+      console.log("You win! Rock beats Scissors");
+      return;
+    }
+  }
+  if (humanChoice === "paper") {
+    if (computerChoice === "scissors") {
+      computerScore++;
+      console.log("You lose! Scissors beats Paper");
+      return;
+    }
+    if (computerChoice === "rock") {
+      humanScore++;
+      console.log("You win! Paper beats Rock");
+      return;
+    }
+  }
+  if (humanChoice === "scissors") {
+    if (computerChoice === "rock") {
+      computerScore++;
+      console.log("You lose! Rock beats Scissors");
+      return;
+    }
+    if (computerChoice === "paper") {
+      humanScore++;
+      console.log("You win! Scissors beat Paper");
+      return;
+    }
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(humanSelection);
+console.log(computerSelection);
+
+playRound(humanSelection, computerSelection);
+
