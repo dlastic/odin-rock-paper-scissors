71d54887e3ade2dda74d70a6cfc3fb2010b3d0a7
dlastic <daniellastic@gmail.com>
2025-01-26 18:00:27 +0100

    Write the logic to get computer and human choice

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+function getComputerChoice() {
+  let choices = ["rock", "paper", "scissors"];
+  let index = Math.floor(Math.random() * 3);
+
+  return choices[index];
+}
+
+function getHumanChoice() {
+  let invalidChoice = true;
+  while (invalidChoice) {
+    choice = prompt("Choose an object (rock, paper or scissors): ");
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+      invalidChoice = false;
+    }
+  }
+  
+  return choice;
+}
+
+console.log(getHumanChoice())
